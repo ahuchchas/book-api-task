4897dacf4abf382ffcf4e89699e28ba84bf7a198
ahuchchas <86414070+ahuchchas@users.noreply.github.com>
2022-08-19 16:48:19 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const loadBookData = () => {
+    const searchBox = document.getElementById('search-box');
+    const searchText = searchBox.value;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBooks(data));
+}
+
+const displayBooks = data => {
+    const bookInfoArea = document.getElementById('display-bookinfo');
+
+    const foundData = document.getElementById('found-showing');
+    foundData.innerText = `Total found: ${data?.num_found}
+    Total showing: ${data?.docs.length}`;
+
+
+    //book name, author if found, first published if found, how much found, how much showing
+
+    data.docs.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('book-card');
+        div.innerHTML = `
+            <p><strong>Book Name:</strong> ${book?.title}${book?.subtitle ? ' - ' + book.subtitle : ''}</p>
+            <p><strong>Author Name:</strong> <i>${book?.author_name ? book.author_name.join(',  ') : "Not found!"}</i></p>
+            <p><strong>First published:</strong> ${book?.first_publish_year ? book.first_publish_year : "Not found!"}</p>
+        `;
+        bookInfoArea.appendChild(div);
+    });
+
+}
